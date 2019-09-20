3c05eefa3a825dff8c5cc6d806ad45881c8b47f4
Vishnu Y S <ysv8152@gmail.com>
2019-09-20 21:39:35 +0530

    Spark query one complete

--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ from django.utils.decorators import method_decorator
 from django.http import HttpResponse
 from spark.sparker import execute_query1
 from time import time
+from json import dumps
+# from IPython import embed
 
 
 # Create your views here.
@@ -16,19 +18,29 @@ class DefaultView(TemplateView):
     @method_decorator(csrf_exempt)
     def post(self, request):
         query = request.POST['query']
+        query_type = ''
+        if 'group' in query.lower():
+            query_type = 'two'
+        elif 'inner' in query.lower():
+            query_type = 'one'
         queryList = query.split()
         queryList = [q for q in queryList if query != '']
-        fromIndex = queryList.index('FROM')
-        innerIndex = queryList.index('INNER')
-        joinIndex = queryList.index('JOIN')
-        onIndex = queryList.index('ON')
-        whereIndex = queryList.index('WHERE')
-        t1, t1_alias = queryList[fromIndex + 1:innerIndex]
-        t2, t2_alias = queryList[joinIndex + 1:onIndex]
-        c1 = queryList[onIndex + 1:whereIndex]
-        c2 = queryList[whereIndex + 1:]
-        spark_start = time()
-        execute_query1(t1, t1_alias, t2, t2_alias, c1, c2)
-        spark_end = time()
+        if query_type == 'one':
+            fromIndex = queryList.index('FROM')
+            innerIndex = queryList.index('INNER')
+            joinIndex = queryList.index('JOIN')
+            onIndex = queryList.index('ON')
+            whereIndex = queryList.index('WHERE')
+            t1, t1_alias = queryList[fromIndex + 1:innerIndex]
+            t2, t2_alias = queryList[joinIndex + 1:onIndex]
+            c1 = queryList[onIndex + 1:whereIndex]
+            c2 = queryList[whereIndex + 1:]
+            spark_start = time()
+            spark_result = execute_query1(t1, t1_alias, t2, t2_alias, c1, c2)
+            spark_end = time()
         spark_time = spark_end - spark_start
-        return HttpResponse(spark_time)
+        end_result = {
+            'spark_time': spark_time,
+            'spark_result': spark_result
+        }
+        return HttpResponse(dumps(end_result))
--- spark/sparker.py
+++ spark/sparker.py
@@ -2,6 +2,7 @@ import os
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from .schemas import *
+import json
 
 
 def execute_query1(t1, t1_alias, t2, t2_alias, c1, c2):
@@ -28,12 +29,28 @@ def execute_query1(t1, t1_alias, t2, t2_alias, c1, c2):
         df2 = spark.read.csv(file_two, header=False, schema=movies_schema)
     elif t2 == 'rating':
         df2 = spark.read.csv(file_two, header=False, schema=rating_schema)
-    df1.show()
-    df2.show()
-    joined = df1.join(df2, df1[attr1] == df2[attr2])
-    joined.show()
-    # c2_attr = c2[0].split('.')[-1]
-    # parameter = int(c2[2]) if c2[2].isnumeric() else c2[2]
-    # print(c2_attr, parameter)
-    # filtered = joined.filter(joined[c2_attr] == parameter)
+    # df1.show()
+    # df2.show()
+    joined = df1.join(df2, attr1)
+    # joined.show()
+    c2_attr = c2[0].split('.')[-1]
+    parameter = int(c2[2]) if c2[2].isnumeric() else c2[2].strip('"').strip("'")
+    operator = c2[1]
+    print("%r %r %r" % (c2_attr, operator, parameter))
+    print(type(c2_attr), type(operator), type(parameter))
+    if operator == '=':
+        filtered = joined.filter(col(c2_attr) == parameter)
+    elif operator == '<':
+        filtered = joined.filter(col(c2_attr) < parameter)
+    elif operator == '>':
+        filtered = joined.filter(col(c2_attr) > parameter)
+    elif operator == '<=':
+        filtered = joined.filter(col(c2_attr) <= parameter)
+    elif operator == '>=':
+        filtered = joined.filter(col(c2_attr) >= parameter)
+    elif operator == '<>':
+        filtered = joined.filter(col(c2_attr) != parameter)
+
     # filtered.show()
+    # print(joined.count(), filtered.count())
+    return filtered.toJSON().map(lambda j: json.loads(j)).collect()
