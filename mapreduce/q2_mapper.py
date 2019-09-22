9270d5ada88ba099cd1f0f4c97b1396680478a10
Vishal Kushwaha <vishsinkush@gmail.com>
2019-09-22 15:32:30 +0530

    query 2 mapreduce initial commit

---
+++ mapreduce/q2_mapper.py
@@ -0,0 +1,14 @@
+
+import sys
+
+group_by_cols = sys.argv[1:]
+
+for line in sys.stdin:
+    line = line.strip().replace('\"','').replace("\'",'')
+    line = line.split(',')
+    
+    key = []
+    for col in group_by_cols:
+        key.append(line[int(col)])
+    print (*key,sep=',',end='\t')
+    print (*line,sep=',')
---
+++ mapreduce/q2_reducer.py
@@ -0,0 +1,22 @@
+import sys
+
+func_name = sys.argv[1].upper()
+func_param = int(sys.argv[2])
+
+
+def function(key):
+    if func_name == 'COUNT':
+
+
+    if func_name == 'MAX':
+    if func_name == 'MIN':
+    if func_name == 'SUM':
+
+
+for line in sys.stdin:
+    line = line.strip()
+    key, value_list = line.split('\t',1)
+    value_list = value_list.split(',')
+
+
+
