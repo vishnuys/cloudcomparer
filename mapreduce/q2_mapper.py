8f5afe1de049ea7b0e3e1c51be29d792a8e62157
Vishal Kushwaha <vishsinkush@gmail.com>
2019-09-22 18:26:10 +0530

    add working query 2 mapreduce files

--- mapreduce/q2_mapper.py
+++ mapreduce/q2_mapper.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3.7
 import sys
 
 group_by_cols = sys.argv[1:]
--- mapreduce/q2_reducer.py
+++ mapreduce/q2_reducer.py
@@ -1,22 +1,61 @@
+#!/usr/bin/env python3.7
 import sys
 
-func_name = sys.argv[1].upper()
+func_name = (sys.argv[1]).upper()
 func_param = int(sys.argv[2])
+having_val_ = sys.argv[3]
 
+current_key = None
+row_list = []
 
-def function(key):
+def transform_and_print(key):
+    func_output_type = 'int'
+    output_row = key.split(',')
     if func_name == 'COUNT':
+        output_row.append(len(row_list))
+    elif func_name == 'MAX':
+        try:
+            #print ('before max: ',end=' ')
+            #print (row_list[func_param])
+            output_row.append(max(list(map(int, [ r[func_param] for r in row_list ]))))
+        except:
+            func_output_type = 'string'
+            output_row.append(max([ r[func_param] for r in row_list ]))
+    elif func_name == 'MIN':
+        try:
+            output_row.append(min(list(map(int, [ r[func_param] for r in row_list ]))))
+        except:
+            func_output_type = 'string'
+            output_row.append(min([ r[func_param] for r in row_list ]))
+    elif func_name == 'SUM':
+        try:
+            output_row.append(sum(list(map(int,[ r[func_param] for r in row_list ]))))
+        except:
+            print("DATA_MISMATCH")
+            quit()
 
-
-    if func_name == 'MAX':
-    if func_name == 'MIN':
-    if func_name == 'SUM':
-
+    if func_output_type == 'int':
+        having_val = int(having_val_)
+    else:
+        having_val = having_val_
+    if output_row[-1] > having_val:
+        print(output_row)
 
 for line in sys.stdin:
     line = line.strip()
     key, value_list = line.split('\t',1)
-    value_list = value_list.split(',')
+    value_list = value_list.strip().split(',')
+
+    if current_key == key:
+        row_list.append(value_list)
+    else:
+        if current_key:
+            transform_and_print(current_key)
+        current_key = key
+        row_list = []
+        row_list.append(value_list)
+transform_and_print(current_key)
+        
 
 
 
